98d097de989c848429d83b7cae359340db276168
Mohammed Amine <aminebenmade1999@gmial.com>
2023-09-22 19:03:46 +0200

    22/9/2023

--- sasprogram.c
+++ sasprogram.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>  /* for while loop */
 int Ntaches;
 int nbrTaches;
-
 
 struct deadline
 {
@@ -19,8 +19,10 @@ struct todoList
     struct deadline delai;
     int sta;
 };
+    struct todoList taches[100];
+      int Ntaches = 0;
 
-void addTache(struct todoList taches[], int Ntaches){
+void addTache(){
     struct todoList nvtache;
     nvtache.ident = Ntaches + 1;
 
@@ -50,35 +52,116 @@ void addTache(struct todoList taches[], int Ntaches){
 }
 
 
-void printTache(struct todoList taches[], int Ntaches){
+void printTache(){
+
 
  for (int i = 0; i < Ntaches; i++)
     {
-        printf("-----tache numero :%d-----\n",i+1);
-        printf("Identifiant : %d\n", taches[i].ident);
-        printf("titre : %s\n", taches[i].titre);
-        printf("description : %s\n", taches[i].description);
-        printf("deadline :     year: %d     month: %d     day: %d\n", taches[i].delai.year, taches[i].delai.month, taches[i].delai.day);
+        printf("-----TACHE NUMERO :%d-----\n",i+1);
+        printf("| Identifiant : %d\n", taches[i].ident);
+        printf("| titre : %s\n", taches[i].titre);
+        printf("| description : %s\n", taches[i].description);
+        printf("| deadline :     year: %d     month: %d     day: %d\n", taches[i].delai.year, taches[i].delai.month, taches[i].delai.day);
         switch(taches[i].sta){
         case 1:
-            printf("statut :1- Todo");
+            printf("| statut :1- Todo\n");
             break;
         case 2:
-            printf("statut :2- Doing");
+            printf("| statut :2- Doing\n");
             break;
         case 3:
-            printf("statut :3- Done");
+            printf("| statut :3- Done\n");
+            break;
+        default:
+            printf("| statut :non trouver\n");
             break;
 
         }
         /*printf("statut : %s\n", taches[i].sta);*/
-        printf("\n");
+        printf("-----TACHE NUMERO :%d-----\n",i+1);
+        printf("\n\n");
+
     }
+
+}
+
+// Fonction pour trier les tâches par titre (tri à bulles)
+void triparAlpha() {
+    struct todoList tittri;
+    for (int i = 0; i < Ntaches - 1; i++) {
+        for (int j = 0; j < Ntaches - i - 1; j++) {
+            if (strcmp(taches[j].titre, taches[j + 1].titre) > 0) {
+                tittri = taches[j];
+                taches[j] = taches[j + 1];
+                taches[j + 1] = tittri;
+            }
+        }
+    }
+    printf("Tâches triées par titre.\n");
 }
 
 
-void modifTache(){}
-void suppTache(){}
+
+
+
+void modifTache() {
+    int identmodif;
+    printf("Entrer l'identifiant de la tache que vous voulez modifier :");
+    scanf("%d", &identmodif);
+
+    // Search for the task with the specified identifier
+
+for (int i = 0; i < Ntaches; i++){
+    if ((taches[i].ident == identmodif)) {
+        int achnbdl;
+        printf("Qu'est-ce que vous voulez modifier :\n");
+        printf("1 - La description\n");
+        printf("2 - Le statut\n");
+        printf("3 - Le deadline\n");
+        printf("Votre choix : ");
+        scanf("%d", &achnbdl);
+
+        switch (achnbdl) {
+            case 1:
+                printf("Modifier la description : ");
+                scanf(" %[^\n]s", taches[i].description);
+                break;
+            case 2:
+                printf("Modifier le statut (1- Todo, 2- Doing, 3- Done) : ");
+                scanf("%d", &taches[i].sta);
+                break;
+            case 3:
+                printf("Modifier le deadline :\n");
+
+                printf("year :");
+                scanf("%d",&taches[i].delai.year);
+                printf("month :");
+                scanf("%d",&taches[i].delai.month);
+                printf("day :");
+                scanf("%d",&taches[i].delai.day);
+            default:
+                printf("Choix non trouver\n");
+                break;
+        }
+    } else {
+        printf("tache avec lidentifiant %d non trouver.\n", identmodif);
+    }
+}
+ }
+
+
+
+void suppTache(){
+    int identsupp;
+    printf("Entrer l'identifiant de la tache que vous voulez supprimer :");
+    scanf("%d", &identsupp);
+    for (int i = 0; i < Ntaches; i++){
+    if ((taches[i].ident == identsupp)){
+
+
+    }
+
+}
 void searchTache(){}
 void statisticTache(){}
 
@@ -102,20 +185,19 @@ int main() {
 
 
 
-    struct todoList taches[100];
-      int Ntaches = 0;
+
     int choix;
 
     while (true) {
 
-        printf("\n1) -----------Ajouter une nouvelle tache----------------");
-        printf("\n2) -----------Ajouter plusieurs nouvelles taches--------");
-        printf("\n3) -----------Afficher la liste de toutes les taches----");
-        printf("\n4) -----------Modifier une tache------------------------");
-        printf("\n5) -----------Supprimer une tache par identifiant-------");
-        printf("\n6) -----------Rechercher les taches---------------------");
-        printf("\n7) -----------Statistiques------------------------------");
-        printf("\n\nVEUILLEZ ENTER VOTRE CHOIX :");
+        printf("\n                     1) -----------Ajouter une nouvelle tache----------------");
+        printf("\n                     2) -----------Ajouter plusieurs nouvelles taches--------");
+        printf("\n                     3) -----------Afficher la liste de toutes les taches----");
+        printf("\n                     4) -----------Modifier une tache------------------------");
+        printf("\n                     5) -----------Supprimer une tache par identifiant-------");
+        printf("\n                     6) -----------Rechercher les taches---------------------");
+        printf("\n                     7) -----------Statistiques------------------------------");
+        printf("\n\n           VEUILLEZ ENTER VOTRE CHOIX :");
         scanf("%d",&choix);
 
         switch(choix){
@@ -137,11 +219,13 @@ int main() {
             }
             break;
         case 3:
-            printf("3) Afficher la liste de toutes les taches\n");
+            printf("3) Afficher la liste de toutes les taches :\n\n");
+            triparAlpha(taches, Ntaches);
             printTache(taches,Ntaches);
             break;
         case 4:
             printf("4) Modifier une tache\n");
+            modifTache();
             break;
         case 5:
             printf("5) Supprimer une tache par identifiant\n");
@@ -157,7 +241,7 @@ int main() {
             }
 
 
-            //system("pause");
+            system("pause");
     }
 
 
